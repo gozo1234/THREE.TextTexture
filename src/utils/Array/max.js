b570754fc5984b84eb9a92f75cb92054a693363e
Sergej Sintschilin <seregpie@gmail.com>
2018-10-24 19:49:16 +0200

    fix Array_max

--- src/utils/Array/max.js
+++ src/utils/Array/max.js
@@ -1,5 +1,3 @@
 export default function(array) {
-	if (array.length > 0) {
-		return array.reduce((maxValue, value) => Math.max(maxValue, value));
-	}
+	return array.reduce((maxValue, value) => Math.max(maxValue, value));
 }
